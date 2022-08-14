4de9fc289659d1bfdda236a2069992c520c8d961
Meridanus <graf__dracula@hotmail.com>
2022-08-14 23:05:49 +0200

    simplify register Block Items

--- src/main/java/com/notker/xps_additions/regestry/AdditionItems.java
+++ src/main/java/com/notker/xps_additions/regestry/AdditionItems.java
@@ -3,33 +3,31 @@ package com.notker.xps_additions.regestry;
 import com.notker.xp_storage.XpStorage;
 import com.notker.xps_additions.XpsAdditions;
 import com.notker.xps_additions.items.Mystical_Candy;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class AdditionItems {
 
+
+    private static BlockItem createBlockItem (Block block) {
+        return new BlockItem(block, new Item.Settings().group(XpStorage.ITEM_GROUP));
+    }
+
+
     // Items
     public static final Item MYSTICAL_CANDY = new Mystical_Candy();
-
-    //Block Items
-    public static final BlockItem SOUL_COPPER_DOOR_ITEM = new BlockItem(AdditionBlocks.SOUL_COPPER_DOOR, new Item.Settings().group(XpStorage.ITEM_GROUP));
-    public static final BlockItem SOUL_COPPER_TRAP_DOOR_ITEM = new BlockItem(AdditionBlocks.SOUL_COPPER_TRAP_DOOR, new Item.Settings().group(XpStorage.ITEM_GROUP));
-    public static final BlockItem SOUL_COPPER_PRESSURE_PLATE_ITEM = new BlockItem(AdditionBlocks.SOUL_COPPER_PRESSURE_PLATE, new Item.Settings().group(XpStorage.ITEM_GROUP));
-    public static final BlockItem SOUL_COPPER_BARS_ITEM = new BlockItem(AdditionBlocks.SOUL_COPPER_BARS, new Item.Settings().group(XpStorage.ITEM_GROUP));
 
     public static void registerItems() {
         //Items
         Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID,"mystical_candy"), MYSTICAL_CANDY);
 
         //BlockItems
-        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_door"), SOUL_COPPER_DOOR_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_trap_door"), SOUL_COPPER_TRAP_DOOR_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_pressure_plate"), SOUL_COPPER_PRESSURE_PLATE_ITEM);
-        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_bars"), SOUL_COPPER_BARS_ITEM);
+        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_door"), createBlockItem(AdditionBlocks.SOUL_COPPER_DOOR));
+        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_trap_door"), createBlockItem(AdditionBlocks.SOUL_COPPER_TRAP_DOOR));
+        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_pressure_plate"), createBlockItem(AdditionBlocks.SOUL_COPPER_PRESSURE_PLATE));
+        Registry.register(Registry.ITEM, new Identifier(XpsAdditions.MOD_ID, "soul_copper_bars"), createBlockItem(AdditionBlocks.SOUL_COPPER_BARS));
     }
 }
