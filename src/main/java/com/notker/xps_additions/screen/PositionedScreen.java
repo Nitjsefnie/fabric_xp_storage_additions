60b163a07c83f09a55a70e061c4cffe6fbe70d67
Meridanus <graf__dracula@hotmail.com>
2022-08-20 22:23:48 +0200

    add Comments

--- src/main/java/com/notker/xps_additions/screen/PositionedScreen.java
+++ src/main/java/com/notker/xps_additions/screen/PositionedScreen.java
@@ -31,39 +31,52 @@ public class PositionedScreen extends HandledScreen<ScreenHandler> {
         int y = ((height - backgroundHeight) / 2) + 6;
         int x = (width - textRenderer.getWidth(Integer.toString(xp))) / 2;
 
-        int xStorage = (width - textRenderer.getWidth(new TranslatableText("block.xps.block_xp_obelisk"))) / 2;
-
-
-        //textRenderer.draw(matrices, "text", 0, 0, 65280);
         renderBackground(matrices);
         super.render(matrices, mouseX, mouseY, delta);
         drawMouseoverTooltip(matrices, mouseX, mouseY);
+
+        //Draw Storage Title
+        int xStorage = (width - textRenderer.getWidth(new TranslatableText("block.xps.block_xp_obelisk"))) / 2;
         textRenderer.draw(matrices, new TranslatableText("block.xps.block_xp_obelisk"), xStorage, y - 15, 4210752);
 
+        //Draw Xp bar Background
 
+        //Vanilla Xp Bar
         RenderSystem.setShaderTexture(0, DrawableHelper.GUI_ICONS_TEXTURE);
-        drawTexture(matrices,  (width - 182) / 2  , y + 5, 0, 64, 182, 5);
+        int v = 64;
+        int barWidth = 182;
+        int barHeight = 5;
 
+        //Custom xp Bar
+        //RenderSystem.setShaderTexture(0, TEXTURE);
+        //int v = 236;
+        //int barWidth = 162;
+        //int barHeight = 5;
 
-        int containerLevel = XpFunctions.getLevelFromExp(xp);
-        int container_excess_xp = xp - XpFunctions.get_total_xp_value_from_level(containerLevel);
-        int container_next_level_xp = XpFunctions.getToNextExperienceLevel(containerLevel);
-        float container_progress = ((1f / container_next_level_xp) * container_excess_xp);
+        int[] color = {0xec00b8, 0x99ff33}; //Purple - Green
 
+        drawTexture(matrices,  (width - barWidth) / 2  , y + 5, 0, v, barWidth, barHeight);
+
+        int level = XpFunctions.getLevelFromExp(xp);
+        int excess_xp = xp - XpFunctions.get_total_xp_value_from_level(level);
+        int next_level_xp = XpFunctions.getToNextExperienceLevel(level);
+        float container_progress = ((1f / next_level_xp) * excess_xp);
+
+        //Draw Xp bar Overlay
         if (container_progress > 0) {
-           int scaledWidth = (int)(container_progress * 183.0F);
-            drawTexture(matrices, (width - 182) / 2 , y + 5, 0, 69, scaledWidth, 5);
+           int scaledWidth = (int)(container_progress * (float)barWidth + 1f);
+            drawTexture(matrices, (width - barWidth) / 2 , y + 5, 0, v + barHeight, scaledWidth, barHeight);
         }
 
         //Draw Level String
-        String string = String.valueOf(containerLevel);
+        String string = String.valueOf(level);
         int levelStringCenter = (width - textRenderer.getWidth(string)) / 2;
 
         textRenderer.draw(matrices, string, (levelStringCenter + 1), y, 0);
         textRenderer.draw(matrices, string, (levelStringCenter - 1), y, 0);
         textRenderer.draw(matrices, string, levelStringCenter, (y + 1), 0);
         textRenderer.draw(matrices, string, levelStringCenter, (y - 1), 0);
-        textRenderer.draw(matrices, string, levelStringCenter, y, 8453920);
+        textRenderer.draw(matrices, string, levelStringCenter, y, color[1]);
     }
 
     @Override
