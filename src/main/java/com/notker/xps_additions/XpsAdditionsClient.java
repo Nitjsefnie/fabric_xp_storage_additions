89fb8192ef05f0a0a70df5511c4aab0371c1b029
Meridanus <96318256+Meridanus@users.noreply.github.com>
2022-12-28 01:40:11 +0100

    Fix Command line Bug When Stripped Logs can have 2 Variants

--- src/main/java/com/notker/xps_additions/XpsAdditionsClient.java
+++ src/main/java/com/notker/xps_additions/XpsAdditionsClient.java
@@ -1,18 +1,21 @@
 package com.notker.xps_additions;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.Multimap;
 import com.notker.xps_additions.items.StaffOfRebark;
 import com.notker.xps_additions.mixin.AxeItemAccessor;
 import com.notker.xps_additions.regestry.AdditionBlocks;
+import com.notker.xps_additions.regestry.AdditionItems;
 import com.notker.xps_additions.screen.PositionedScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
+import net.minecraft.block.Block;
 import net.minecraft.client.gui.screen.ingame.HandledScreens;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.text.Text;
+import net.minecraft.util.registry.Registry;
 
-import java.util.AbstractMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class XpsAdditionsClient implements ClientModInitializer {
     @Override
@@ -25,10 +28,24 @@ public class XpsAdditionsClient implements ClientModInitializer {
 
         ClientPlayConnectionEvents.JOIN.register((packetListener, packetSender, mc) -> {
             if (StaffOfRebark.STRIPPED_BLOCKS == null) {
-                System.out.println("Null on Client");
-                StaffOfRebark.STRIPPED_BLOCKS = AxeItemAccessor.getStrip().entrySet().stream()
-                .map((e) -> new AbstractMap.SimpleImmutableEntry<>(e.getKey(), e.getValue()))
-                .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
+                // get Actual Name of Staff
+                final String staffName = Text.translatable(AdditionItems.STAFF_OF_REBARK.getTranslationKey()).getString();
+                // Start Logging Process
+                XpsAdditions.LOGGER.info("Null on Server -> Add Log/Stripped variants to " + staffName);
+
+                // Create Map Builder
+                ImmutableMultimap.Builder<Block, Block> builder = ImmutableMultimap.builder();
+                // Get Blocks from Mixin and put it switched in to Map
+                AxeItemAccessor.getStrip().forEach((block, strippedBlock) -> builder.put(strippedBlock, block));
+                // Build the Map
+                Multimap<Block, Block> strippedBlock_Block = builder.build();
+                // Add Map to Staff
+                StaffOfRebark.STRIPPED_BLOCKS = strippedBlock_Block;
+
+                // InfoLogg Blocks
+                strippedBlock_Block.forEach((block, block2) -> XpsAdditions.LOGGER.info(staffName + " add: " + Registry.BLOCK.getId(block) + " to " +  Registry.BLOCK.getId(block2)));
+                // End Staff Logging
+                XpsAdditions.LOGGER.info("Finished adding " + strippedBlock_Block.size() + " Blocks to " + staffName);
             }
         });
     }
